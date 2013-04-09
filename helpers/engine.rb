afa809d59b5d247b8c45b16e5635256e318f8cfa
Adam Jetmalani <adam.k.jet@gmail.com>
2013-04-09 12:06:03 -0700

    found myself stark naked in south america, shaving a yak

--- helpers/engine.rb
+++ helpers/engine.rb
@@ -1,13 +1,27 @@
 module EngineHelper
   def hn_items
-    page = Nokogiri::HTML(open("https://news.ycombinator.com/"))
+    page = Nokogiri::HTML(open("https://news.ycombinator.com/news"))
 
     items = []
-    page.css("td[class='title'] a").each do |thing|
-      item = { :title => thing.text, :url => thing["href"] }
+
+    list = page.xpath("/html/body/center/table/tr[3]/td/table").children
+
+    list.each do |thing|
+      a = thing.css("td[class='title']")
+      b = thing.css("td[class='subtext']")
+      item = { :title => a.text,
+               :url => a["href"],
+               :score => }
+
+
+    page.css("td[class='title'] a").each do |top|
+      item = { :title => top.text, :url => top["href"] }
       items << item unless item[:title] == "more"
     end
-    
+
+    page.css("td[class='subtext']").each do |bottom|
+      item[:score] = bottom
+
     HNListing.new({
       :time_gotten => Time.now,
       :posts => items
